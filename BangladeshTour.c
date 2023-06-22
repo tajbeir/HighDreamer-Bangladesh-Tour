3cc77aec89a17324257c925f5a4db76bf7a434b6
Jahidul Islam <136675591+jahidnub@users.noreply.github.com>
2023-06-22 22:39:07 +0600

    Update BangladeshTour.c

--- BangladeshTour.c
+++ BangladeshTour.c
@@ -1,34 +1,59 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<conio.h>
 #include<string.h>
 
 
 void traveller(){
-   printf("\n\n\t\t\t\t\t...................\n");
-   printf("\t\t\t\t\t: BANGLADESH TOUR :\n");
-   printf("\t\t\t\t\t:.................:\n\n\n");
-   printf("\tPLEASE ENTER YOUR RIGHT INFORMATION\n\n");
-   
+   int press;
+   printf("\n\n\t\t\t\t\t\t...................\n");
+   printf("\t\t\t\t\t\t: BANGLADESH TOUR :\n");
+   printf("\t\t\t\t\t\t:.................:\n\n\n\n");
+   printf("\t\t\t\t PLEASE ENTER YOUR RIGHT INFORMATION AS A TRAVELLER\n\n\n\n");
+   printf("\t\t\t\t press 1 for exit.....\n");
+   scanf("%d",&press);
+   system("cls");
+
+       switch(press)
+        {
+            case 1:
+                registration();
+                break;
+        }
+
+
 }
 void guide(){
-   printf("\n\n\t\t\t\t\t...................\n");
-   printf("\t\t\t\t\t: BANGLADESH TOUR :\n");
-   printf("\t\t\t\t\t:.................:\n\n\n");
-   printf("\tPLEASE ENTER YOUR RIGHT INFORMATION\n\n");
+   int press;
+   printf("\n\n\t\t\t\t\t\t...................\n");
+   printf("\t\t\t\t\t\t: BANGLADESH TOUR :\n");
+   printf("\t\t\t\t\t\t:.................:\n\n\n\n");
+   printf("\t\t\t\t PLEASE ENTER YOUR RIGHT INFORMATION AS A GUIDE\n\n\n\n");
+   printf("\t\t\t\t press 1 for exit.....\n");
+   scanf("%d",&press);
+   system("cls");
+
+       switch(press)
+        {
+            case 1:
+                registration();
+                break;
+        }
 
 }
 
 void registration(){
   int press;
   while(1){
-    printf("\n\n\t\t\t\t\t...................\n");
-    printf("\t\t\t\t\t: BANGLADESH TOUR :\n");
-    printf("\t\t\t\t\t:.................:\n\n\n");
-    printf("\n\t 1. REGISTRATION AS A TRAVELLER \n");
-    printf("\t 2. REGISTRATION AS A GUIDE\n ");
-    printf("\t 3.Exit\n\n");
-    printf("   press any key from 1 to 3.....\n");
-    scanf("%d",&press);
+   printf("\n\n\t\t\t\t\t\t...................\n");
+   printf("\t\t\t\t\t\t: BANGLADESH TOUR :\n");
+   printf("\t\t\t\t\t\t:.................:\n\n\n\n");
+   printf("\n\t\t\t\t 1. REGISTRATION AS A TRAVELLER \n\n");
+   printf("\t\t\t\t 2. REGISTRATION AS A GUIDE\n\n ");
+   printf("\t\t\t\t 3.Exit\n\n\n\n");
+   printf("\t\t\t\t press any key from 1 to 3.....\n");
+   scanf("%d",&press);
+   system("cls");
         switch(press)
         {
             case 1:
@@ -38,31 +63,42 @@ void registration(){
                 guide();
                 break;
              case 3:
-                printf("\n\tExiting Program.");
                dashBoard();
         }
   }
 
 }
 void logIn(){
-     printf("\n\n\t\t\t\t\t...................\n");
-     printf("\t\t\t\t\t: BANGLADESH TOUR :\n");
-     printf("\t\t\t\t\t:.................:\n\n\n");
-     printf("\n\n\t\t\t WELCOME SIR FOR BANGLADESH TOUR\n");
-     printf("\t\t\tPLEASE ENTER YOUR NAME AND PASSWORD\n");
+    int press;
+    printf("\n\n\t\t\t\t\t\t...................\n");
+    printf("\t\t\t\t\t\t: BANGLADESH TOUR :\n");
+    printf("\t\t\t\t\t\t:.................:\n\n\n\n");
+    printf("\n\n\t\t\t\t WELCOME SIR FOR BANGLADESH TOUR\n\n");
+    printf("\t\t\t\t PLEASE ENTER YOUR NAME AND PASSWORD\n\n\n\n");
+    printf("\t\t\t\t press 1 for exit.....\n");
+    scanf("%d",&press);
+    system("cls");
+
+       switch(press)
+        {
+            case 1:
+                dashBoard();
+                break;
+        }
 
 
 }
 void dashBoard(){
- int press;
- while(1){
-   printf("\n\n\t\t\t\t\t...................\n");
-   printf("\t\t\t\t\t: BANGLADESH TOUR :\n");
-   printf("\t\t\t\t\t:.................:\n\n\n");
-   printf("\t 1.REGISTRATION\n");
-   printf("\t 2.LOG IN\n\n");
-   printf("   press any key from 1 or 2.....\n");
+    int press;
+    while(1){
+   printf("\n\n\t\t\t\t\t\t...................\n");
+   printf("\t\t\t\t\t\t: BANGLADESH TOUR :\n");
+   printf("\t\t\t\t\t\t:.................:\n\n\n\n");
+   printf("\t\t\t\t 1.REGISTRATION\n\n");
+   printf("\t\t\t\t 2.LOG IN\n\n\n\n");
+   printf("\t\t\t\t press any key from 1 or 2.....\n");
    scanf("%d",&press);
+   system("cls");
         switch(press)
         {
             case 1:
@@ -74,20 +110,24 @@ void dashBoard(){
 
         }
     }
+
 }
 
 int main(){
 
  int press;
- while(1) {
-   printf("\n\n\t\t\t\t\t...................\n");
-   printf("\t\t\t\t\t: BANGLADESH TOUR :\n");
-   printf("\t\t\t\t\t:.................:\n\n\n");
-   printf("\n\n\t\t\t TRAVEL IS AN INVESTMENT IN YOURSELF\n\n\n");
-   printf("\t\t\t WELCOME TO BANGLADESH, THE LAND OF BEAUTY\n\n\n\n");
-   printf("   press 1 to continue.....\n");
+ while(1)
+    {
+   printf("\n\n\t\t\t\t\t\t...................\n");
+   printf("\t\t\t\t\t\t: BANGLADESH TOUR :\n");
+   printf("\t\t\t\t\t\t:.................:\n\n\n\n");
+   printf("\n\n\t\t\t\t TRAVEL IS AN INVESTMENT IN YOURSELF\n\n\n");
+   printf("\t\t\t\t WELCOME TO BANGLADESH, THE LAND OF BEAUTY\n\n\n\n\n");
+   printf("\t\t\t\t press 1 to continue.....\n");
    scanf("%d",&press);
-        switch(press)
+   system("cls");
+
+       switch(press)
         {
             case 1:
                 dashBoard();
